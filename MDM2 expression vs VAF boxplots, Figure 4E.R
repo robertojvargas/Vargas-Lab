83e3fdd1d524fd17e033f86919830c460da25dd5
petty1284 <82487665+petty1284@users.noreply.github.com>
2022-08-10 13:22:07 -0400

    Add files via upload

--- MDM2 expression vs VAF boxplots, Figure 4E.R
+++ MDM2 expression vs VAF boxplots, Figure 4E.R
@@ -8,7 +8,7 @@ library(reshape2)
 #Set working directory 
 setwd("/Users/pettya/Desktop/R Analysis")
 
-#Boxplot of MDM2 expression versus TP53 VAF (Figure 4E)
+#Create new TCGA patient table with MDM2 expression data
 ##Read in excel file with all TCGA patient data
 endometrial <- read_xlsx("Final TCGA with p53 and RT annotation - R.xlsx", 1)
 
@@ -23,9 +23,20 @@ names(endometrial_MDM2)[52] <- "MDM2_Expression"
 ##Create categorical VAF column from Allelic Frequency data and convert to ordered factor
 endometrial_MDM2$VAF[endometrial_MDM2$Allele.Freq..T. <= 0.5] <- "Low"
 endometrial_MDM2$VAF[endometrial_MDM2$Allele.Freq..T. > 0.5] <- "High"
-endometrial_MDM2$VAF[endometrial_MDM2$Allele.Freq..T. == "WT"] <- "WT"
+endometrial_MDM2$VAF[endometrial_MDM2$`P53 Mut` == 0] <- "WT"
 endometrial_MDM2$VAF <- factor(endometrial_MDM2$VAF, levels = c("WT", "Low", "High"))
 
+
+
+#One-way ANOVA and Tukey's Multiple Comparisons stats on MDM2 expression versus VAF
+MDM2_VAF <- aov(MDM2_Expression ~ VAF, endometrial_MDM2)
+MDM2_MC <- TukeyHSD(MDM2_VAF)
+summary(MDM2_VAF)
+MDM2_MC
+
+
+
+#Boxplot of MDM2 expression versus TP53 VAF (Figure 4E)
 ##Create colored boxplot with black dots
 qb <- ggplot(endometrial_MDM2,  aes(x= VAF, y= MDM2_Expression)) + theme_classic() +
   labs(x= NULL, y= "MDM2 Expression") + theme(axis.title.y = element_text(size= 18, face= "bold")) + 
@@ -42,8 +53,6 @@ jpeg("VAF vs MDM2 expression boxplot, box color.jpeg", width= 720, height= 720, 
 print(qb)
 
 dev.off()
-
-
 
 ##Create gray boxplot with colored dots
 qb <- ggplot(endometrial_MDM2,  aes(x= VAF, y= MDM2_Expression)) + theme_classic() +
